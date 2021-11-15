67047b53955bcbba8626e4cfe3ec3af48a280a96
CeoNabux <lualreye@gmail.com>
2021-11-15 13:54:39 -0500

    extended colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,17 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#87C7C9",
+        secondary: "#FF98C9",
+        textColor: "#4E6F71",
+        offerColor: "#8D7DA3",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
