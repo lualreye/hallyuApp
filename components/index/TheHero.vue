ab1334ad053a191b83031102eba45cac890a8311
CeoNabux <lualreye@gmail.com>
2021-11-17 12:29:25 -0500

    hero template fixed

--- components/index/TheHero.vue
+++ components/index/TheHero.vue
@@ -2,17 +2,21 @@
   <div class="w-full mx-auto flex justify-around items-center flex-wrap px-4 py-4">
     <!-- LEFT CONTAINER, AQUI CONTAMOS CON EL COPYRIGHT DEL HERO -->
     <div class="w-full lg:w-1/2">
-      <h1 class="text-5xl font-junegull text-secondary text-center lg:text-left">Hallyu Store</h1>
-      <div class="w-full lg:w-48 mt-5 flex flex-col lg:items-start">
+      <h1 class="text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-junegull text-secondary text-center lg:text-left">Hallyu Store</h1>
+      <div class="w-full lg:w-72 xl:w-96 mt-5 flex flex-col items-center lg:items-start">
         <div class="flex justify-center items-center">
-          <p class="text-textColor text-2xl">Tu puerta a</p>
-          <div class="relative ml-1">
-            <p class="text-textColor text-2xl relative z-10">Corea</p>
+          <p class="text-textColor text-2xl md:text-3xl lg:text-4xl xl:text-5xl">Tu puerta a</p>
+          <div class="relative ml-1 lg:ml-3 xl:ml-4">
+            <p class="text-textColor text-2xl md:text-3xl lg:text-4xl xl:text-5xl relative z-10">Corea</p>
             <span
               class="
                 z-0
-                w-10
+                w-14
+                lg:w-20
+                xl:w-28
                 h-3
+                lg:h-4
+                xl:h-5
                 bg-yellow
                 absolute
                 rounded-lg
@@ -23,14 +27,19 @@
           </div>
         </div>
         <div class="flex justify-center items-center">
-          <p class="text-textColor text-2xl">desde</p>
-          <div class="relative ml-1">
-            <p class="text-textColor text-2xl relative z-10">Ecuador</p>
+          <p class="text-textColor text-2xl md:text-3xl lg:text-4xl xl:text-5xl">desde</p>
+          <div class="relative ml-1 lg:ml-3 xl:ml-4">
+            <p class="text-textColor text-2xl md:text-3xl lg:text-4xl xl:text-5xl relative z-10">Ecuador</p>
             <span
               class="
                 z-0
-                w-14
+                w-16
+                md:w-24
+                lg:w-28
+                xl:w-40
                 h-3
+                lg:h-4
+                xl:h-5
                 bg-yellow
                 absolute
                 rounded-lg
