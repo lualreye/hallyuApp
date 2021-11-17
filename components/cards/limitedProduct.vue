4875a50d43f0ac843d06f3d545a414ad220b0aa3
CeoNabux <lualreye@gmail.com>
2021-11-17 12:51:53 -0500

    limited products added to index, preparing for creating template

---
+++ components/cards/limitedProduct.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    Esta es una tarjeta
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="w-full max-w-screen-2xl mx-auto">
     <the-hero :image="heroImage" />
+    <the-limited-products />
   </div>
 </template>
 
 <script>
 import TheHero from '~/components/index/TheHero.vue'
+import TheLimitedProducts from '~/components/index/TheLimitedProducts.vue'
 export default {
   components: {
-    TheHero
+    TheHero,
+    TheLimitedProducts
   },
   data: () => ({
     heroImage: require('~/static/images/idols/han.jpg')
