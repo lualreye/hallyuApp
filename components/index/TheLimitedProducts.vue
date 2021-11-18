f6f4b0821f31b5d020c15230a0d09a371c55c489
CeoNabux <lualreye@gmail.com>
2021-11-18 15:03:38 -0500

    title added

--- components/index/TheLimitedProducts.vue
+++ components/index/TheLimitedProducts.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="w-full mt-10 px-4">
-    <div class="w-full flex justify-center items-center h-14">
+    <div class="w-full flex justify-center lg:justify-start items-center h-14">
       <div class="parenthesis w-11 h-11 relative">
         <first-type />
       </div>
-      <h2 class="text-4xl font-junegull text-textColor">Hallyu on Clock</h2>
-      <div class="w-5">Image</div>
+      <h2 class="text-xl sm:text-4xl font-junegull text-textColor">Hallyu on Clock</h2>
+      <div class="parenthesis-two w-11 h-11 relative">
+        <first-type />
+      </div>
     </div>
   </div>
 </template>
@@ -22,5 +24,8 @@ export default {
 <style scoped>
 .parenthesis {
   transform: rotate(50.95deg);
+}
+.parenthesis-two {
+  transform: rotate(200.95deg);
 }
 </style>
