5fa960f0043f32a36ce53e3e360aea40751aa15f
CeoNabux <lualreye@gmail.com>
2021-11-18 14:58:05 -0500

    parenthesis component added for titles

--- components/index/TheLimitedProducts.vue
+++ components/index/TheLimitedProducts.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="w-full">
-    Hola
+  <div class="w-full mt-10 px-4">
+    <div class="w-full flex justify-center items-center h-14">
+      <div class="parenthesis w-11 h-11 relative">
+        <first-type />
+      </div>
+      <h2 class="text-4xl font-junegull text-textColor">Hallyu on Clock</h2>
+      <div class="w-5">Image</div>
+    </div>
   </div>
 </template>
+
+<script>
+import FirstType from "~/components/parenthesis/FirstType.vue";
+export default {
+  components: {
+    FirstType,
+  },
+};
+</script>
+
+<style scoped>
+.parenthesis {
+  transform: rotate(50.95deg);
+}
+</style>
