4b12b31b2003b1409e752cbd57ede659262d9666
CeoNabux <lualreye@gmail.com>
2021-11-22 12:34:34 -0500

    the favourites section the favourites section added to index page

--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,19 @@
   <div class="w-full max-w-screen-2xl mx-auto">
     <the-hero :image="heroImage" />
     <the-limited-products :urgentProducts="urgentProducts" />
+    <the-favourites />
   </div>
 </template>
 
 <script>
 import TheHero from "~/components/index/TheHero.vue";
 import TheLimitedProducts from "~/components/index/TheLimitedProducts.vue";
+import TheFavourites from '~/components/index/TheFavourites.vue';
 export default {
   components: {
     TheHero,
     TheLimitedProducts,
+    TheFavourites,
   },
   data: () => ({
     heroImage: require("~/static/images/idols/han.jpg"),
