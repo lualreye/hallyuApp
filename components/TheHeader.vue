4e4fa8c52bae85f43469b12ac1b0eab63b4f938b
CeoNabux <lualreye@gmail.com>
2021-11-22 12:12:49 -0500

    the favourites section component teamplate fo the section mentioned created, and imagotipo addded to static folder, in order to use favicon in the browser

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -3,7 +3,7 @@
     <div class="mx-auto w-full max-w-screen-2xl flex justify-between items-center px-4">
       <icon-button iconName="menu" classes="bg-primary" />
       <figure class="w-28 sm:w-36 md:w-40 lg:w-48 flex justify-center items-center">
-        <img src="~/static/images/logo/imagotipo.png" alt="hallyu-store" />
+        <img src="~/static/images/logo/logotipo.png" alt="hallyu-store" />
       </figure>
       <div class="w-32 md:w-48 max-w-xs flex justify-between items-center">
         <icon-button iconName="userAccount" classes="bg-primary p-1" />
---
+++ components/index/TheFavourites.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="w-full mt-16 px-4 py-6">
+    Hola, saludos desde favourites
+  </div>
+</template>
