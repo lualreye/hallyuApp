51d853e07a1928d8b565d192123288e90048f260
CeoNabux <lualreye@gmail.com>
2021-12-01 10:01:22 -0500

    nuxt config title modified

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'hallyuApp',
+    title: 'hallyu Store',
     htmlAttrs: {
       lang: 'es'
     },
