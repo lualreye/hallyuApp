a08caa61b54d7573c20565cddfe0df3dffab8ecf
CeoNabux <lualreye@gmail.com>
2021-12-03 14:46:48 -0500

    Cta component, button component cta component almost ready as the model, button component in global was added and ready for implmenting the function for signUp

---
+++ components/global/HButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <button class="w-full py-4 rounded-2xl" :class="color" @click="$emit('click', $event)">
+    <p class="text-lg font-open text-white text-center">
+      {{ name }}
+    </p>
+  </button>
+</template>
+
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    buttonColor: {
+      type: String,
+      required: null
+    }
+  },
+  computed: {
+    color() {
+      const color = 'bg-primary'
+      if(!this.buttonColor.length) {
+        return color
+      }
+      return `bg-`+this.buttonColor
+    }
+  }
+}
+</script>
--- components/index/TheCta.vue
+++ components/index/TheCta.vue
@@ -6,8 +6,22 @@
     </div>
     <!-- SECCION DE CTA -->
     <div class="w-full flex justify-center items-center relative z-10 py-4">
-      <div class="bg-white rounded-lg w-72 lg:w-3/4">
-        Este es el cta
+      <div class="bg-white rounded-xl w-72 lg:w-3/4 py-4 shadow-lg">
+        <p class="mb-3 text-textColor text-lg font-medium text-center font-open">
+          Regístrate y forma parte de nuestra comunidad
+        </p>
+        <p class="mb-3 text-3xl font-junegull text-center text-primary">
+          HALLYU
+        </p>
+        <p class="mb-3 font-light text-sm font-open text-center text-textColor">
+          Recibe notifiaciones de ofertas especiales
+        </p>
+        <p class="mb-3 text-textColor text-lg font-medium text-center font-open">
+          Obtén beneficios en tu primera compar
+        </p>
+        <div class=" mx-auto w-44 justify-center items-center">
+          <h-button name="Registrarse" buttonColor="secondary" @click="probando" />
+        </div>
       </div>
     </div>
   </div>
@@ -15,9 +29,16 @@
 
 <script>
 import CtaDoodle from "~/components/svgs/CtaDoodle.vue"
+import HButton from "~/components/global/HButton.vue"
 export default {
   components: {
-    CtaDoodle
+    CtaDoodle,
+    HButton
+  },
+  methods:{
+    probando() {
+      console.log('probando desde cta, estoy vivooooo')
+    }
   }
 }
 </script>
