8d16f63c402babe4ecb520c0e41b1b08b950c014
CeoNabux <lualreye@gmail.com>
2021-12-08 10:31:05 -0500

    cta component cta template coded as the model, waiting for programming the cta button

--- components/index/TheCta.vue
+++ components/index/TheCta.vue
@@ -1,26 +1,57 @@
 <template>
-  <div class="w-full mt-16 px-4 py-6 relative">
-    <!-- TRABAJAMOS EN BACKGROUND DE LA SECCION -->
-    <div class="w-full absolute z-0">
-      <cta-doodle />
-    </div>
-    <!-- SECCION DE CTA -->
-    <div class="w-full flex justify-center items-center relative z-10 py-4">
-      <div class="bg-white rounded-xl w-72 lg:w-3/4 py-4 shadow-lg">
-        <p class="mb-3 text-textColor text-lg font-medium text-center font-open">
-          Regístrate y forma parte de nuestra comunidad
-        </p>
-        <p class="mb-3 text-3xl font-junegull text-center text-primary">
-          HALLYU
-        </p>
-        <p class="mb-3 font-light text-sm font-open text-center text-textColor">
-          Recibe notifiaciones de ofertas especiales
-        </p>
-        <p class="mb-3 text-textColor text-lg font-medium text-center font-open">
-          Obtén beneficios en tu primera compar
-        </p>
-        <div class=" mx-auto w-44 justify-center items-center">
-          <h-button name="Registrarse" buttonColor="secondary" @click="probando" />
+  <div class="w-full mt-16">
+    <div class="w-full">
+      <!-- SECCION DE CTA -->
+      <div
+        class="
+          w-full
+          flex
+          justify-center
+          items-center
+          py-8
+          my-auto
+          cta-image
+        "
+        :style="{ backgroundImage: 'url(' + bgImage + ')' }"
+      >
+        <div class="bg-white rounded-xl w-72 lg:w-3/4 py-4 shadow-lg">
+          <p
+            class="
+              mb-3
+              text-textColor text-lg
+              font-medium
+              text-center
+              font-open
+            "
+          >
+            Regístrate y forma parte de nuestra comunidad
+          </p>
+          <p class="mb-3 text-3xl font-junegull text-center text-primary">
+            HALLYU
+          </p>
+          <p
+            class="mb-3 font-light text-sm font-open text-center text-textColor"
+          >
+            Recibe notifiaciones de ofertas especiales
+          </p>
+          <p
+            class="
+              mb-3
+              text-textColor text-lg
+              font-medium
+              text-center
+              font-open
+            "
+          >
+            Obtén beneficios en tu primera compar
+          </p>
+          <div class="mx-auto w-44 justify-center items-center">
+            <h-button
+              name="Registrarse"
+              buttonColor="secondary"
+              @click="probando"
+            />
+          </div>
         </div>
       </div>
     </div>
@@ -28,17 +59,18 @@
 </template>
 
 <script>
-import CtaDoodle from "~/components/svgs/CtaDoodle.vue"
-import HButton from "~/components/global/HButton.vue"
+import HButton from "~/components/global/HButton.vue";
 export default {
   components: {
-    CtaDoodle,
-    HButton
+    HButton,
   },
-  methods:{
+  data: () => ({
+    bgImage: require("~/static/images/backgrounds/Doodle.svg")
+  }),
+  methods: {
     probando() {
-      console.log('probando desde cta, estoy vivooooo')
-    }
-  }
-}
+      console.log("probando desde cta, estoy vivooooo");
+    },
+  },
+};
 </script>
