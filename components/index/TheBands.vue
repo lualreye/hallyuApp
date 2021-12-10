97bb0e90853313bd7b80dd258b2fc28fd396067a
CeoNabux <lualreye@gmail.com>
2021-12-10 15:18:46 -0500

    the bands section component is ready to receive data and ready according to the model

--- components/index/TheBands.vue
+++ components/index/TheBands.vue
@@ -5,7 +5,15 @@
       class="w-full flex flex-col justify-center lg:justify-start items-start"
     >
       <div
-        class="w-full flex justify-center lg:justify-start items-center h-14"
+        class="
+          w-full
+          flex flex-col
+          justify-center
+          lg:justify-start
+          items-center
+          md:items-baseline
+          h-20
+        "
       >
         <div class="flex justify-center items-end">
           <h2
@@ -20,10 +28,28 @@
           >
             Hallyu Stock
           </h2>
-          <p class=" ml-2 font-open font-bold text-xl text-textColor">por tu</p>
+          <p class="ml-2 font-open font-bold text-xl text-textColor">por tu</p>
+          <div class="parenthesis-two w-11 h-11 relative">
+            <tenth-type />
+          </div>
         </div>
-        <div class="parenthesis-two w-11 h-11 relative">
-          <tenth-type />
+        <div class="flex justify-center items-end">
+          <p
+            class="
+              text-xl
+              sm:text-4xl
+              text-right
+              sm:text-left
+              font-junegull
+              text-textColor
+              md:ml-48
+            "
+          >
+            Banda
+          </p>
+          <p class="ml-2 font-open font-bold text-xl text-right text-textColor">
+            favorita
+          </p>
         </div>
       </div>
       <div
@@ -45,7 +71,16 @@
       <div
         v-for="(band, i) in bands"
         :key="i"
-        class="flex justify-center items-center w-full sm:w-1/2 md:w-1/3 lg:w-1/4 mb-8"
+        class="
+          flex
+          justify-center
+          items-center
+          w-full
+          sm:w-1/2
+          md:w-1/3
+          lg:w-1/4
+          mb-8
+        "
       >
         <band-card :bandImage="band.bandImage" :bandName="band.bandName" />
       </div>
