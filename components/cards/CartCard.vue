5ff654182fe1ff5797ccc3d590aae033713fa59d
luis <lualreye@gmail.com>
2022-01-07 18:28:31 -0500

    cart component cart card added to the card according to the model, and responsive working fine preparing to receive data from the index fetch

--- components/cards/CartCard.vue
+++ components/cards/CartCard.vue
@@ -2,30 +2,30 @@
   <div class="w-full mb-4">
     <div class="flex justify-between items-start">
       <!-- LEFT SECTION OF THE CARD -->
-      <div class="flex justify-start items-start w-2/3">
-        <figure class="w-1/4 ml-3 flex justify-center items-center">
+      <div class="flex justify-start items-start w-3/4">
+        <figure class="w-32 bg-secondary p-2 rounded-3xl mr-2 flex justify-center items-center">
           <img
             :src="image"
             :alt="productName"
-            class="flex justify-center items-center"
+            class="rounded-xl flex justify-center items-center"
           />
         </figure>
-        <div class="flex flex-col justify-between items-start">
-          <p class="w-full text-textColor mb-3 text-xl font-bold font-open">
+        <div class="flex flex-col py-1 justify-between items-start">
+          <p class="w-full text-textColor mb-3 text-base font-bold font-open">
             {{ productName }}
           </p>
-          <p class="text-base text-textColor font-open">
+          <p class="text-sm text-textColor font-open">
             {{ description.slice(0, 50) }}
           </p>
         </div>
       </div>
       <!-- RIGHT SECTION OF THE CARD -->
-      <div class="flex flex-col justify-between">
+      <div class="flex py-1 flex-col justify-between items-end w-1/4">
         <p class="text-xl text-textColor font-open font-bold">
           {{ price }}
         </p>
         <p class="text-secondary font-bold">
-          {{ discount }} %
+          - {{ discount }} %
         </p>
       </div>
     </div>
