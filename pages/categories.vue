768e71fed3d59e8a6fda491e41e6622a71644fb1
luis <lualreye@gmail.com>
2022-01-18 18:10:53 -0500

    categories view the categories nav has scroll effect in mobile size

--- pages/categories.vue
+++ pages/categories.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="w-full">
       <nav class="w-full max-w-xl mx-auto px-2 my-4">
-        <ul class="w-full flex justify-around items-center px-2 overflow-auto py-4">
+        <ul class="w-full flex justify-between items-center px-2 overflow-auto py-4">
           <li v-for="(category, i) in categories" :key="i" class="flex-shrink-0 text-textColor font-junegull mx-2">{{ category.name }}</li>
         </ul>
       </nav>
