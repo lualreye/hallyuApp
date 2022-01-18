b6a74fcbd8de0282717c6c24b77c201cab2436a6
luis <lualreye@gmail.com>
2022-01-18 18:08:55 -0500

    categories view categories added and rendering according to the model

--- pages/categories.vue
+++ pages/categories.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <div class="w-full">
-      <nav class="w-full px-2">
-        <ul class="w-full flex justify-around items-center">
-          <li class="text-lg font-junegull">{{ categoria }}</li>
+      <nav class="w-full max-w-xl mx-auto px-2 my-4">
+        <ul class="w-full flex justify-around items-center px-2 overflow-auto py-4">
+          <li v-for="(category, i) in categories" :key="i" class="flex-shrink-0 text-textColor font-junegull mx-2">{{ category.name }}</li>
         </ul>
       </nav>
     </div>
