cfb45a1a1db888bd07caee9ab835241eb6a2ebbd
luis <lualreye@gmail.com>
2022-01-18 17:56:09 -0500

    preparing categories view

--- pages/categories.vue
+++ pages/categories.vue
@@ -1,5 +1,25 @@
 <template>
   <div>
-    Saludos desde categorias
+    <div class="w-full">
+      <nav class="w-full px-2">
+        <ul class="w-full flex justify-around items-center">
+          <li class="text-lg font-junegull">{{ categoria }}</li>
+        </ul>
+      </nav>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    categories: [
+      { name: 'moda' },
+      { name: 'k-pop' },
+      { name: 'snack' },
+      { name: 'accesorios' },
+      { name: 'peluches' },
+    ]
+  })
+}
+</script>
