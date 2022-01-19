0b980d17897c7bf38e037c3dca850e34c206a247
luis <lualreye@gmail.com>
2022-01-18 19:14:45 -0500

    product card fixing template according to the model, preparing everything for the render

---
+++ components/cards/ProductCard.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class=" w-56 h-96">
+    <div class="relative w-full h-full">
+      <img
+        :src="productImage"
+        alt="productName"
+        class="border-2 border-secondary rounded-3xl w-56 h-96"
+      />
+      <div class="w-full h-full flex-flex-col justify-between items-center">
+        <div
+          class="
+            absolute
+            top-2
+            right-2
+            w-full
+            flex flex-col
+            justify-between
+          "
+        >
+          <div class="w-full flex justify-end items-center">
+            <button
+              class="
+                w-8
+                h-8
+                p-1
+                shadow-xl
+                rounded-full
+                bg-gray-100
+                flex
+                justify-center
+                items-center
+              "
+            >
+              <h-icon
+                name="like"
+                :class="{
+                  'text-gray-300': !isLiked,
+                  'text-secondary': isLiked,
+                }"
+              />
+            </button>
+          </div>
+        </div>
+        <div class="flex justify-between items-center">
+          <p class="text-white font-junegull">
+            $ {{ price }}
+          </p>
+          <button class="w-8 h-8 flex justify-center items-center shadow-xl bg-primary rounded-full">
+            <h-icon name='cart' class="text-gray-100" />
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HIcon from "../global/HIcon.vue";
+export default {
+  components: { HIcon },
+  data: () => ({
+    productImage: require("@/static/images/idols/han.jpg"),
+  }),
+};
+</script>
+
+<style scoped>
+.productImage {
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
--- pages/categories.vue
+++ pages/categories.vue
@@ -2,24 +2,46 @@
   <div>
     <div class="w-full">
       <nav class="w-full max-w-xl mx-auto px-2 my-4">
-        <ul class="w-full flex justify-between items-center px-2 overflow-auto py-4">
-          <li v-for="(category, i) in categories" :key="i" class="flex-shrink-0 text-textColor font-junegull mx-2">{{ category.name }}</li>
+        <ul
+          class="
+            w-full
+            flex
+            justify-between
+            items-center
+            px-2
+            overflow-auto
+            py-4
+          "
+        >
+          <li
+            v-for="(category, i) in categories"
+            :key="i"
+            class="flex-shrink-0 text-textColor font-junegull mx-2"
+          >
+            {{ category.name }}
+          </li>
         </ul>
       </nav>
+    </div>
+    <div class="w-full py-4 px-2">
+      <product-card />
     </div>
   </div>
 </template>
 
 <script>
+import ProductCard from "../components/cards/ProductCard.vue";
+
 export default {
+  components: { ProductCard },
   data: () => ({
     categories: [
-      { name: 'moda' },
-      { name: 'k-pop' },
-      { name: 'snack' },
-      { name: 'accesorios' },
-      { name: 'peluches' },
-    ]
-  })
-}
+      { name: "moda" },
+      { name: "k-pop" },
+      { name: "snack" },
+      { name: "accesorios" },
+      { name: "peluches" },
+    ],
+  }),
+};
 </script>
