47bebe4889e2f70504358d101e38eb21f4471abe
luis <lualreye@gmail.com>
2022-02-18 14:40:03 -0500

    feat: firebase added to the project

---
+++ static/js/firebaseConfig.js
@@ -0,0 +1,30 @@
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+import { getAuth } from 'firebase/auth'
+import { getStorage } from 'firebase/storage'
+import { getFirestore } from 'firebase/firestore'
+
+
+const firebaseConfig = {
+  apiKey: process.env.NUXT_ENV_KEY,
+  authDomain: process.env.NUXT_ENV_AUTHDOMAIN,
+  projectId: process.env.NUXT_ENV_PROJECTID,
+  storageBucket: process.env.NUXT_ENV_STORAGEBUCKET,
+  messsagingSenderId: process.env.NUXT_ENV_MESSAGING,
+  appId: process.env.NUXT_ENV_APPID,
+  measumerementId: process.env.NUXT_ENV_MEASUREMENT,
+}
+
+const app = initializeApp(firebaseConfig)
+const fireAnalytics = getAnalytics(app)
+const fireAuth = getAuth(app)
+const fireDataBase = getFirestore(app)
+const fireStore = getStorage(app)
+
+export {
+  app,
+  fireAnalytics,
+  fireAuth,
+  fireDataBase,
+  fireStore
+}
