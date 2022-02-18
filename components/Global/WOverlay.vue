7e5fe89deddca352512b5eb6f6ac6370e3d3306d
luis <lualreye@gmail.com>
2022-02-18 09:13:38 -0500

    feat: new overlay added for sign in and sign up

---
+++ components/Global/WOverlay.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="
+      w-screen
+      h-screen
+      bg-gray-100
+      opacity-70
+      flex
+      justify-center
+      items-center
+      absolute
+      z-40
+      top-0
+      left-0
+      right-0
+      bottom-0
+    "
+  @click="closeModal"></div>
+</template>
+
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+export default {
+  computed: {
+    ...mapGetters('user', ['getModal'])
+  },
+  methods: {
+    ...mapActions('user', ['showModal']),
+    closeModal() {
+      if(getModal) {
+        this.showeModal(false)
+      }
+    }
+  }
+}
+</script>
