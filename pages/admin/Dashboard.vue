177c558d1adecac706724733e39793a89745b022
lualreye <lualreye@gmail.com>
2022-04-13 01:47:47 -0500

    feat: rendering chart

--- pages/admin/Dashboard.vue
+++ pages/admin/Dashboard.vue
@@ -4,7 +4,7 @@
     <div class="w-full flex justify-center items-center flex-wrap">
       <!-- SALES -->
       <div class="w-full lg:w-3/5 flex justify-center items-center p-3">
-        <DashboardCardsIncomesGraph />
+        <DashboardCardsIncomesGraph :chart-data="chartData" />
       </div>
       <!-- INCOME CARDS -->
       <div class="w-full lg:w-2/5 flex justify-center items-center p-3"></div>
@@ -14,6 +14,28 @@
 
 <script>
 export default {
-  layout: "app"
-}
+  layout: "app",
+  data: () => ({
+    chartData: {
+      labels: [
+        "January",
+        "February",
+        "March",
+        "January",
+        "February",
+        "March",
+        "January",
+        "February",
+        "March",
+      ],
+      datasets: [
+        {
+          label: "Meses",
+          backgroundColor: "#87C7C9",
+          data: [4000, 2000, 1200, 4000, 2000, 1200, 4000, 2000, 1200],
+        },
+      ],
+    },
+  }),
+};
 </script>
