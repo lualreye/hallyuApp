c363ec511b10d387c4c571d97b0f075f573701e9
lualreye <lualreye@gmail.com>
2022-04-12 23:04:06 -0500

    fix:header sizes

--- layouts/app.vue
+++ layouts/app.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="w-full flex justify-center items-center">
     <DashboardMenu />
-    <Nuxt class="flex-1" />
+    <div class="flex-1 h-screen flex flex-col justify-center items-center">
+      <DashboardHeader />
+      <Nuxt class="flex-1" />
+    </div>
   </div>
 </template>
