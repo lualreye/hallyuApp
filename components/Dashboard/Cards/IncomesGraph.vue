9e8675bb7a789d1e359b8e8adc16edd65c5d16a9
lualreye <lualreye@gmail.com>
2022-04-13 01:47:11 -0500

    feat: graph receiving props

--- components/Dashboard/Cards/IncomesGraph.vue
+++ components/Dashboard/Cards/IncomesGraph.vue
@@ -3,6 +3,55 @@
     class="w-full shadow-md border border-aquemarine flex flex-col justify-between items-center rounded-lg p-6"
   >
     <p class="text-hBlack w-full">Ventas históricas</p>
-    <div>Soy la grafica</div>
+    <div class="w-full">
+      <LineChartGenerator
+        :chart-options="chartOptions"
+        :chart-data="chartData"
+        :height="chartHeight"
+      />
+    </div>
   </div>
 </template>
+
+<script>
+import { Line as LineChartGenerator } from "vue-chartjs/legacy";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  LinearScale,
+  CategoryScale,
+  PointElement,
+} from "chart.js";
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement
+);
+
+export default {
+  components: {
+    LineChartGenerator,
+  },
+  props: {
+    chartData: {
+      type: Object,
+      required: true,
+    },
+  },
+  data: () => ({
+    chartOptions: {
+      responsive: true,
+      maintainAspectRatio: false,
+    },
+    chartHeight: 200,
+  }),
+};
+</script>
