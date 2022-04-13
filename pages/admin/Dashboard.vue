8f4c001c378f21d8b1ae7ed1cff221b92b1c7d90
lualreye <lualreye@gmail.com>
2022-04-13 00:22:52 -0500

    add: first section

--- pages/admin/Dashboard.vue
+++ pages/admin/Dashboard.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    Estamos en el dashboard
+  <div class="w-full">
+    <!-- INCOMES SECTION -->
+    <div class="w-full flex justify-center items-center flex-wrap">
+      <!-- SALES -->
+      <div class="w-full lg:w-3/5 flex justify-center items-center p-3">
+        <DashboardCardsIncomesGraph />
+      </div>
+      <!-- INCOME CARDS -->
+      <div class="w-full lg:w-2/5 flex justify-center items-center p-3"></div>
+    </div>
   </div>
 </template>
 
