38b7feb6f77077d3c0817cfe5f975b3d364f0df8
lualreye <lualreye@gmail.com>
2022-04-13 02:25:31 -0500

    created: incomes resume

---
+++ components/Dashboard/Cards/SalesResume.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="w-full h-24 border border-bg-200 shadow-md bg-background rounded-lg p-3">
+    <div class="w-full mb-3">
+      <p class="text-hBlack text-sm font-open">
+        Ingresos
+      </p>
+      <p class="text-hBlack text-xs font-light font-open">
+        Semanal
+      </p>
+    </div>
+    <p class="text-hBlack font-semibold font-open">
+      $ {{ monthIncome }}
+    </p>
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: {
+    monthIncome: {
+      type: Number,
+      required: true,
+      default: 0
+    }
+  }
+}
+</script>
