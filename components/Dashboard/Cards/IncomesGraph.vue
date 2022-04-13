d437d507fc60f4524668cd3600a34d503c8c61f8
lualreye <lualreye@gmail.com>
2022-04-13 00:22:21 -0500

    created: incomes graph

---
+++ components/Dashboard/Cards/IncomesGraph.vue
@@ -0,0 +1,8 @@
+<template>
+  <div
+    class="w-full shadow-md border border-aquemarine flex flex-col justify-between items-center rounded-lg p-6"
+  >
+    <p class="text-hBlack w-full">Ventas históricas</p>
+    <div>Soy la grafica</div>
+  </div>
+</template>
