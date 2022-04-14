510738202a44df94621d2f1978e5ecb3898874dc
lualreye <lualreye@gmail.com>
2022-04-13 23:46:13 -0500

    fix: responsive layout

--- layouts/app.vue
+++ layouts/app.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="flex justify-center items-start">
+  <div class="w-full flex justify-center 2xl:justify-start items-start">
     <DashboardMenu />
-    <div class="h-full w-11/12 sm:w-full flex flex-col justify-center items-center">
+    <div class="h-full w-11/12 sm:w-11/12 md:w-11/12 lg:w-full max-w-screen-2xl flex flex-col justify-center items-center">
       <DashboardHeader />
-      <Nuxt class="flex-1" />
+      <Nuxt class="w-11/12 sm:w-full" />
     </div>
   </div>
 </template>
