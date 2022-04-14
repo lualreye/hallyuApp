96e0e80548ece2e31de855b57bf88a478829d25f
lualreye <lualreye@gmail.com>
2022-04-14 16:15:25 -0500

    feat: reveiving props

--- components/Dashboard/Cards/MostSelled.vue
+++ components/Dashboard/Cards/MostSelled.vue
@@ -5,8 +5,32 @@
     <p class="text-lg font-open text-hBlack w-full">Top 10 más vendidos</p>
     <div class="w-full flex flex-col justify-center items-center">
       <div class="w-full flex justify-center items-center">
-        <DashboardCardsVotedProduct />
+        <DashboardCardsVotedProduct
+          v-for="(product, index) in products"
+          :key="index"
+          :productImage="product.image"
+          :productName="product.name"
+          :salesTotal="salesTotal"
+          :salesnumber="product.salesNumber"
+          :color="product.color"
+        />
       </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    products: {
+      type: Array,
+      required: true
+    },
+    salesTotal: {
+      type: Number,
+      required: true,
+      default: 500
+    }
+  }
+};
+</script>
