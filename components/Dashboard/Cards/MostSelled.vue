51ac4d6cf6ce23c07f7c14420f6368cfe9dbb6fc
lualreye <lualreye@gmail.com>
2022-04-14 16:04:03 -0500

    feat: heighest selled product

---
+++ components/Dashboard/Cards/MostSelled.vue
@@ -0,0 +1,12 @@
+<template>
+  <div
+    class="w-full h-80 flex flex-col justify-between items-center rounded-lg shadow-md bg-background p-6"
+  >
+    <p class="text-lg font-open text-hBlack w-full">Top 10 más vendidos</p>
+    <div class="w-full flex flex-col justify-center items-center">
+      <div class="w-full flex justify-center items-center">
+        <DashboardCardsVotedProduct />
+      </div>
+    </div>
+  </div>
+</template>
