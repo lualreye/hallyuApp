444f6d38be70a250f22cb75666a77fc690d9d576
lualreye <lualreye@gmail.com>
2022-04-16 02:01:37 -0500

    refact: app menu moved and renamed

--- layouts/app.vue
+++ layouts/app.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full flex justify-center 2xl:justify-start items-start">
-    <DashboardMenu />
+    <AppGlobalMenu />
     <div class="flex-grow shrink w-8/12 flex flex-col max-w-screen-2xl relative">
       <AppGlobalHeader />
       <Nuxt class="p-1 md:px-4 md:py-1 overflow-auto" />
