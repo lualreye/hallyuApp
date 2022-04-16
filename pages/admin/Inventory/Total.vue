25400c935deff0aa98aa106af151b24b37190073
lualreye <lualreye@gmail.com>
2022-04-16 04:57:09 -0500

    feat: label added

--- pages/admin/Inventory/Total.vue
+++ pages/admin/Inventory/Total.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>
-    Soy el inventario total
+  <div class="w-full">
+    <!-- LABELS -->
+    <div
+      class="w-full flex justify-center itesm-center divide-x divide-textColor"
+    >
+      <div class="w-4/12 flex justify-center items-center text-hBlack font-open">Productos</div>
+      <div class="w-2/12 flex justify-center items-center text-hBlack font-open">Categoría</div>
+      <div class="w-1/12 flex justify-center items-center text-hBlack font-open">Stock</div>
+      <div class="w-1/12 flex justify-center items-center text-hBlack font-open">Precio</div>
+      <div class="w-1/12 flex justify-center items-center text-hBlack font-open">Publicado</div>
+      <div class="w-1/12 flex justify-center items-center text-hBlack font-open">Oferta</div>
+      <div class="w-2/12 flex justify-center items-center text-hBlack font-open"></div>
+    </div>
   </div>
 </template>
