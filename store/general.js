37f3e9d845000969dddb5ba34cf7c513df3496ae
lualreye <lualreye@gmail.com>
2022-04-18 02:13:37 -0500

    feat: uploading image

--- store/general.js
+++ store/general.js
@@ -1,6 +1,14 @@
-
-
-
+import { fireStorage, fireDataBase } from "../static/js/firebaseConfig";
+import {
+  getDoc,
+  doc,
+  updateDoc,
+  collection,
+  query,
+  where,
+  setDoc,
+} from "firebase/firestore";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 const state = () => ({
   heroImages: [],
@@ -17,27 +25,37 @@ const mutations = {
     state.heroImages = heroImages;
   },
   SET_IMAGE(state, heroImage) {
-    state.heroImages.push(heroImage)
-  }
+    state.heroImages.push(heroImage);
+  },
 };
 
 const actions = {
   async uploadHeroImages({ commit }, payload) {
     try {
-      console.log(payload)
-      commit("SET_IMAGE", image)
+      const storage = fireStorage
+      const fileName = payload.name
+      const imageRef = ref(storage, `heroImages/${fileName}`);
+      await uploadBytes(imageRef, payload)
+      const imageUrl = await getDownloadURL(imageRef)
+      const database = fireDataBase
+      const heroImageRef = doc(collection(database, "heroImages"))
+      const heroImage = {
+        name: payload.name.split(".").shift(),
+        image: imageUrl
+      }
+      await setDoc(heroImageRef, heroImage)
+      commit("SET_IMAGE", heroImage);
     } catch (err) {
       console.error("CANNOT_UPLOAD_HERO_IMAGE", err);
     }
   },
-  async getHeroImages({commit}) {
+  async getHeroImages({ commit }) {
     try {
-
-      commit("SET_IMAGES", heroImages)
-    } catch(err) {
-      console.error("CANNOT_GET_HERO_IMAGES", err)
+      commit("SET_IMAGES", heroImages);
+    } catch (err) {
+      console.error("CANNOT_GET_HERO_IMAGES", err);
     }
-  }
+  },
 };
 
 export { state, getters, mutations, actions };
