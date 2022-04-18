8543301dd027d66f2173a7f71787576da4582a29
lualreye <lualreye@gmail.com>
2022-04-18 00:03:51 -0500

    fix: open button

--- components/General/Categories.vue
+++ components/General/Categories.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="w-full flex flex-col justify-center items-start mt-3">
-    <div
+    <button
       class="w-96 flex justify-start items-center border border-text-gray-400 rounded-lg p-2"
+      @click="openModal"
     >
-      <button @click="openModal">
+      <div>
         <div class="w-6 h-6 flex justify-center items-center mr-3">
           <GlobalHIcon :name="getIcon" class="text-textColor" />
         </div>
-      </button>
+      </div>
       <p class="text-textColor font-open">Categorías</p>
-    </div>
+    </button>
     <div v-if="isOpen" class="w-full mt-3">Estamos subiendo imagenes</div>
   </div>
 </template>
--- components/General/HeroImages.vue
+++ components/General/HeroImages.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="w-full flex flex-col justify-center items-start mt-3">
-    <div class="w-96 flex justify-start items-center border border-text-gray-400 rounded-lg p-2">
-      <button @click="openModal">
+    <button class="w-96 flex justify-start items-center border border-text-gray-400 rounded-lg p-2" @click="openModal">
+      <div>
         <div class="w-6 h-6 flex justify-center items-center mr-3">
           <GlobalHIcon :name="getIcon" class="text-textColor" />
         </div>
-      </button>
+      </div>
       <p class="text-textColor font-open">Subir Portadas</p>
-    </div>
+    </button>
     <div v-if="isOpen" class="w-full mt-3">Estamos subiendo imagenes</div>
   </div>
 </template>
