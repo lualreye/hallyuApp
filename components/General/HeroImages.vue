f5f9d691462ed098630ce0aeaeeec89aa0151d10
lualreye <lualreye@gmail.com>
2022-04-18 00:12:33 -0500

    feat: image input added

--- components/General/HeroImages.vue
+++ components/General/HeroImages.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="w-full flex flex-col justify-center items-start mt-3">
-    <button class="w-96 flex justify-start items-center border border-text-gray-400 rounded-lg p-2" @click="openModal">
+    <button
+      class="w-96 flex justify-start items-center border border-text-gray-400 rounded-lg p-2"
+      @click="openModal"
+    >
       <div>
         <div class="w-6 h-6 flex justify-center items-center mr-3">
           <GlobalHIcon :name="getIcon" class="text-textColor" />
@@ -8,7 +11,22 @@
       </div>
       <p class="text-textColor font-open">Subir Portadas</p>
     </button>
-    <div v-if="isOpen" class="w-full mt-3">Estamos subiendo imagenes</div>
+    <div v-if="isOpen" class="w-full max-w-md mt-3 p-2">
+      <div class="w-full flex flex-wrap justify-between items-center">
+        <p class="text-hBlack font-open">Imagenes de Bienvenida</p>
+        <div
+          class="w-40 relative bg-lightPink flex justify-center items-center py-2 rounded-lg mt-3 sm:mt-0"
+        >
+          <input type="file" class="absolute w-full h-full z-60 opacity-0" />
+          <div class="w-full flex justify-center items-center">
+            <div class="w-6 h-6 flex justify-center items-center mr-2">
+              <GlobalHIcon name="upload" class="text-textColor" />
+            </div>
+            <p class="text-textColor font-open">Subir imagen</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
