c16957656d9da0a07e7cebe02b1e9c5a9b59dac7
lualreye <lualreye@gmail.com>
2022-04-19 14:56:29 -0500

    feat: render posts in card

---
+++ components/Blog/Cards/List.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="w-full flex justify-center items-center rounded-lg shadow-md p-2 bg-background"
+  >
+    <div class="w-full flex flex-col justify-center items-center">
+      <div class="w-full flex justify-between items-center">
+        <p class="text-base font-open text-textColor">Título del blog</p>
+        <p class="text-base font-open text-textColor">Fecha de publicación</p>
+      </div>
+      <div class="bg-primary h-px w-full" />
+      <div class="w-full flex flex-col justify-center items-center my-2">
+        <BlogDetail />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    posts: {
+      type: Array,
+      required: true,
+      default: [],
+    },
+  },
+};
+</script>
