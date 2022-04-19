d032e1e5b2ea6b5c5427263cba1355ebfdf3d7e8
lualreye <lualreye@gmail.com>
2022-04-19 15:24:19 -0500

    feat: editing box

--- pages/admin/Blog.vue
+++ pages/admin/Blog.vue
@@ -2,18 +2,47 @@
   <div class="w-full flex flex-col shrink justify-center items-center">
     <!-- BUTTON EDIT POST -->
     <div class="w-full flex justify-start items-center my-2">
-      <button class="w-auto mr-2 flex justify-between items-center" @click="editPost">
+      <button
+        class="w-auto mr-2 flex justify-between items-center"
+        @click="editPost"
+      >
         <div class="w-6 h-6 flex justify-center items-center">
           <GlobalHIcon name="plus" class="text-textColor" />
         </div>
         <p class="text-textColor font-open">nuevo blog</p>
       </button>
     </div>
-    <div v-if="isEditingPost" class="w-full flex justify-center items-center my-2">
-      Edicion de post
+    <!-- POST EDITOR -->
+    <div
+      v-if="isEditingPost"
+      class="w-full max-w-3xl flex justify-center items-center my-2"
+    >
+      <div class="w-full flex flex-col justify-center items-center">
+        <!-- EDITOR -->
+        <div class="w-full flex flex-wrap justify-center items-center">
+          imagenes y cuerpo
+        </div>
+        <!-- BUTTONS -->
+        <div class="w-full flex justify-end items-center my-2">
+          <button
+            class="w-44 py-2 rounded-lg flex justify-center text-textColor items-center mr-2 bg-lightPink"
+            @click="editPost"
+          >
+            Cancelar
+          </button>
+          <button
+            class="w-44 py-2 rounded-lg flex justify-center items-center text-textColor bg-primary"
+            @click="savePost"
+          >
+            Guardar
+          </button>
+        </div>
+      </div>
     </div>
     <!-- POSTS LIST -->
-    <div class="w-full max-w-2xl flex flex-col justify-center items-center my-2">
+    <div
+      class="w-full max-w-2xl flex flex-col justify-center items-center my-2"
+    >
       <BlogCardsList />
     </div>
   </div>
@@ -24,6 +53,12 @@ export default {
   layout: "app",
   data: () => ({
     isEditingPost: false,
+    title: "",
+    image: {
+      url: null,
+      object: null,
+    },
+    body: "",
   }),
   methods: {
     editPost() {
@@ -32,6 +67,14 @@ export default {
       } else {
         this.isEditingPost = true;
       }
+    },
+    savePost() {
+      const post = {
+        image: this.image.object,
+        title: this.title,
+        body: this.body,
+      };
+      console.log(post);
     },
   },
 };
