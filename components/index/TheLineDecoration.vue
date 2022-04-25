a81dc69e297f58e2d448c6dcba0d0d7e0f58620b
lualreye <lualreye@gmail.com>
2022-04-25 18:54:00 -0500

    create: index line decoration

---
+++ components/index/TheLineDecoration.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="w-full h-7 flex justify-center items-center" :style="{backgroundColor: color}">
+    <div class="w-full max-w-md fkex justify-center items-center">
+      <p class="text-background font-open">
+        {{ spanishTest }}
+      </p>
+      <p class="text-background font-open">
+        {{ koreanTest }}
+      </p>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: {
+    spanishText: {
+      type:String,
+      required: true,
+      default: Hallyu
+    },
+    koreanText: {
+      type:String,
+      required: true,
+      default: Coreano
+    },
+  }
+}
+</script>
