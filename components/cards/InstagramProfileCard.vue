267dba70257d4760a34355fe8df168e706c91ed1
Luis Reyes <lualreye@gmail.com>
2022-06-26 13:29:36 -0500

    fix: instagram profiles styles

--- components/cards/InstagramProfileCard.vue
+++ components/cards/InstagramProfileCard.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="w-full flex justify-center items-center">
     <div class="relative w-full">
-      <figure class="w-full justify-center items-center mx-auto">
-        <img :src="instagramUserImage" :alt="instagramUserName" class="mx-auto">
+      <figure class="w-full h-48 justify-center items-center mx-auto rounded-md">
+        <img :src="instagramUserImage" :alt="instagramUserName" class="w-full h-48 object-cover object-center mx-auto rounded-md">
       </figure>
       <a :href="`${instagramUserName}/`" target="_blank" class="absolute bottom-2 left-2 py-1 px-2 rounded-lg bg-white flex justify-center items-center">
         <p class="font-light text-secondary text-sm font-open">@{{ getName }}</p>
