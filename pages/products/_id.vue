ac06142ae44eab1925d292cb3cdbaeb1974156cb
Luis Reyes <lualreye@gmail.com>
2022-07-04 04:53:55 -0500

    feat: product detail template

--- pages/products/_id.vue
+++ pages/products/_id.vue
@@ -1,15 +1,40 @@
 <template>
   <div class="py-4 px-2 max-w-screen-xl mx-auto my-9">
-    <div class="w-full md:w-1/2 flex jusify-center items-center">
-      <div class="w-full flex flex-row md:flex-col justify-center items-center">
-        <figure v-for="(image, index) in getProduct.images" :key="index" class="w-20 h-16 flex justify-center items-center border-2 border-primary rounded-lg">
-          <img :src="image" alt="">
+    <div class="w-full flex flex-wrap">
+      <div class="w-full md:w-1/2 flex flex-col-reverse md:flex-row justify-center items-center p-1">
+        <div class="w-full md:w-1/5 flex flex-row md:flex-col justify-center items-center">
+          <figure v-for="(image, index) in getProduct.images" :key="index" class="w-16 h-16 m-2 flex justify-center items-center border-2 border-primary rounded-lg">
+            <img :src="image" alt="" class="object-cover object-center w-20 rounded-md">
+          </figure>
+        </div>
+        <figure class="w-full md:w-4/5 border-2 border-primary rounded-md flex justify-center items-center">
+          <img :src="getProduct.thumbnail" alt="" class="w-full object-cover object-center rounded-md">
         </figure>
-        <figure class="w-full "></figure>
       </div>
-    </div>
-    <div class="w-full md:w-1/2 flex jusify-center items-center">
-
+      <div class="w-full md:w-1/2 p-2">
+        <p class="text-textColor text-base font-open text-left">
+          {{ getProduct.name }}
+        </p>
+        <p class="text-textColor text-2xl font-junegull text-left">
+          {{ getProduct.price }}
+        </p>
+        <div v-if="getProduct.clothes.sizes.length" class="w-full flex flex-col">
+          <p class="text-textColor text-base font-open text-left">
+            Tallas
+          </p>
+          <p v-for="(size, index) in getProduct.clothes.sizes" :key="index" class="text-textColor text-base font-open text-left">
+            {{ size }}
+          </p>
+        </div>
+        <div class="w-full flex flex-col">
+          <p class="text-textColor text-base font-open text-left">
+            Color
+          </p>
+          <p v-for="(item, index) in getProduct.clothes.colors" :key="index" class="text-textColor text-base font-open text-left">
+            {{ item.name }}
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -24,14 +49,6 @@ export default {
       return this.$route.params.id
     }
   },
-  // watch: {
-  //   getParams(newVal, oldVal) {
-  //     if(newVal !== oldVal) {
-  //       this.clearProductsByCategory()
-  //       this.fetchProductsByCategory(this.$route.params.id)
-  //     }
-  //   }
-  // },
   mounted() {
     this.fetchProduct(this.$route.params.id)
   },
