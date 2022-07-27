39ae5430bd57fc942f2d16c11f2a2ebbf1a26d80
Luis Reyes <lualreye@gmail.com>
2022-07-27 15:36:22 -0500

    remove: layout

--- layouts/app.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="w-full flex justify-center 2xl:justify-start items-start">
-    <AppGlobalMenu />
-    <div class="flex-grow shrink w-8/12 flex flex-col relative">
-      <AppGlobalHeader />
-      <Nuxt class="p-1 md:px-4 md:py-1 overflow-auto max-w-screen-2xl" />
-    </div>
-  </div>
-</template>
